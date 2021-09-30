e0273e850a9e42ed1ee320cfce6027be88742173
Zakaria Hasan <43992167+zakariahasan@users.noreply.github.com>
2021-09-30 14:47:46 +0300

    Update SQL-Server.sql

--- SQL-Server.sql
+++ SQL-Server.sql
@@ -1,9 +1,14 @@
--- To view the all tables in the database
+-- view the all tables in the database
 SELECT * 
 FROM INFORMATION_SCHEMA.TABLES
 ORDER BY TABLE_SCHEMA
 
--- To view the all columns in the database
+-- view the all columns in the database
 SELECT * 
 FROM INFORMATION_SCHEMA.COLUMNS
 ORDER BY TABLE_SCHEMA, TABLE_NAME
+-- View only table columns
+SELECT COLUMN_NAME 
+FROM INFORMATION_SCHEMA.COLUMNS
+WHERE TABLE_NAME = 'S_Purchasing'
+ORDER BY TABLE_SCHEMA,  TABLE_NAME
