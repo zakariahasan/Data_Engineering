75e1b34c242bd3d42ee12cc3757d80a44b397d27
Zakaria Hasan <43992167+zakariahasan@users.noreply.github.com>
2022-04-17 15:50:44 +0300

    Update csvFileManipulation.ps1

--- PowerShell/csvFileManipulation.ps1
+++ PowerShell/csvFileManipulation.ps1
@@ -1,2 +1,4 @@
 # Removing header from CSV file
 Get-Content Your_File.csv | select -Skip 1 | Set-Content Your_FileBis.csv
+# or
+Your_Data_In_Pipe | ConvertTo-Csv -NoTypeInformation | select -Skip 1 | Set-Content File_Without_Headers.csv
