71da4c2af3732ca588e1ffceb29ffeba0182c6b7
Zakaria Hasan <43992167+zakariahasan@users.noreply.github.com>
2022-07-20 11:41:03 +0300

    Update Dynamic SQL.sql

--- Dynamic SQL.sql
+++ Dynamic SQL.sql
@@ -7,3 +7,10 @@ SET  @sql = 'SELECT * FROM ' + QUOTENAME('dbo') + '.' + QUOTENAME(@tableName)
 PRINT @sql
 
 EXECUTE sp_executesql @sql
+
+
+-- Parameterized SQL query over dynamic SQL query
+
+-- What is a parameterized SQL query?
+-- It is used for better performance, high efficiency and prevention of SQL injection vulnerability. 
+-- Before going further, let us have a brief introduction to SQL injection.
