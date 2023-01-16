5bdfabcdde77d7007969068e35adcb1358cdf187
Zakaria Hasan <43992167+zakariahasan@users.noreply.github.com>
2023-01-16 23:41:42 +0200

    Update Calendar_month_to_fiscal_perion.py

--- Calendar_month_to_fiscal_perion.py
+++ Calendar_month_to_fiscal_perion.py
@@ -13,3 +13,15 @@ print(map_month_to_fiscal_period(6)) # Output: 1
 print(map_month_to_fiscal_period(11)) # Output: 6
 print(map_month_to_fiscal_period(12)) # Output: 7
 print(map_month_to_fiscal_period(1)) # Output: 8
+
+
+
+
+'''
+This code defines a function called map_month_to_fiscal_period that takes in an integer representing a calendar month (1-12) and 
+returns the corresponding fiscal period.
+It uses a simple if-else statement. If the month is greater or equal than 6, it subtracts 5 from the month and returns the result. 
+If not, it adds 7 to the month and returns the result.
+
+This way, June is the first month of the fiscal period, July is the second and so on until May which is the 12th month of the fiscal period.
+'''
